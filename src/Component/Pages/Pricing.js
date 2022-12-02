928e895b963c19c4b6e0484d0b9af36f23388aa1
Sanjay <sanjaykumarme40@gmail.com>
2022-12-02 18:29:06 +0530

    calendly-app

--- src/Component/Pages/Pricing.js
+++ src/Component/Pages/Pricing.js
@@ -1,10 +1,296 @@
 import React from 'react'
+import './Pricing.css'
 
+// import './Individuals.css'
 const Pricing = () => {
   return (
-    <div>
-      <h1>pricing</h1>
-    </div>
+    <>
+  
+  <div><h1 className='Pick'>Pick the perfect plan for your team</h1> 
+         </div>
+
+         <div className='oneLine'>
+         <div><p className='Billed'>Billed monthly</p></div>
+
+         <div><h6 className='annually'>Billed annually</h6></div>
+
+         <div><h5 className='Save'>Save 20% with annual plans</h5></div>
+         </div>
+
+         {/* card */}
+
+<div className='online_desplay'>
+
+<div className="card w-50">
+  <div className="card-body">
+    <h5 className="card-title">BASIC</h5>
+
+        <div><h3 className='Always'>Always Free</h3></div>
+
+    <p className="card-text">
+    When you just want the basics
+    </p>
+    <a href="https://calendly.com/app/signup" className="btn btn-primary">
+      Get Started
+    </a>
+  </div>
+</div>
+
+{/* card 2 */}
+<div className="card w-50">
+  <div className="card-body">
+    <h5 className="card-title">ESSENTIALS</h5>
+
+        <div><h3 className='Always'>$8
+ /seat/mo</h3></div>
+
+    <p className="card-text">
+    When you need powerful scheduling automation
+    </p>
+    <a href="https://calendly.com/app/signup" className="btn btn-primary">
+       Start Essentials
+    </a>
+  </div>
+</div>
+{/* card 3 */}
+<div className="card w-51">
+  <div className="card-body">
+    <h5 className="card-title">PROFESSIONAL</h5>
+
+        <div><h3 className='Always'>$12
+ /seat/mo</h3></div>
+
+    <p className="card-text">
+    When you  customization, integrations,features
+    </p>
+    <a href="https://calendly.com/app/signup" className="btn btn-primary23">
+       Start Professional
+    </a>
+  </div>
+</div>
+{/* card 4 */}
+<div className="card w-52">
+  <div className="card-body">
+    <h5 className="card-title">TEAMS</h5>
+
+        <div><h3 className='Always'>$16
+ /seat/mo</h3></div>
+
+    <p className="card-text">
+    When you just want the basics
+    </p>
+    <a href="https://calendly.com/app/signup" className="btn btn-primary">
+       Try for free
+    </a>
+  </div>
+</div>
+{/* card 5 */}
+<div className="card blue234">
+  <div className="card-body">
+    <h5 className="ENTERPRISE">ENTERPRISE</h5>
+
+        <div><h3 className='Contact_Us'>Contact Us</h3></div>
+
+    <p className="card-text2">
+    When your 30+ member team needs advanced security, control, and support
+    </p>
+    <a href="https://calendly.com/app/signup1" className="btn2 btn-primary">
+    Contact Us
+    </a>
+  </div>
+</div>
+
+</div>
+
+
+
+{/* part 3 */}
+
+<div className='online23'>
+
+<div className='line_by_line'>
+
+<div><h5 className='FEATURES2'>FEATURES </h5>
+         
+         </div>
+
+         <div><h3 className='Features'>Features by plan</h3></div>
+   
+          <div className='Find'>
+        <p className='subscription'>Find the subscription that makes the most sense for you or your team</p> 
+        </div> 
+
+        </div>
+  {/* card */}
+
+
+
+  <div className="card w-5112">
+  <div className="card-body">
+    <h5 className="basic1">BASIC</h5>
+
+        <div><h3 className='free1'>Free</h3></div>
+
+    
+    <a href="https://calendly.com/app/signup" className="btn btn-primary1">
+       Sign up
+    </a>
+  </div>
+</div>
+
+
+<div className="card w-5112">
+  <div className="card-body">
+    <h5 className="basic1">ESSENTIALS</h5>
+
+        <div><h3 className='free1'>$8</h3></div>
+
+    
+    <a href="https://calendly.com/app/signup" className="btn btn-primary1">
+      Sign up
+    </a>
+  </div>
+</div>
+
+<div className="card w-5112">
+  <div className="card-body">
+    <h5 className="basic1">PROFESSIONAL</h5>
+
+        <div><h3 className='free1'>$12</h3></div>
+
+    
+     <div className='hew1'> <a href="https://calendly.com/app/signup" className="btn23 btn-primary2">
+    Sign up
+    </a></div>
+
+  </div>
+</div>
+
+<div className="card w-5112">
+  <div className="card-body">
+    <h5 className="basic1">TEAMS</h5>
+
+        <div><h3 className='free1'>$16</h3></div>
+
+    
+    <a href="https://calendly.com/app/signup" className="btn btn-primary1">
+    Sign up
+    </a>
+  </div>
+</div>
+
+<div className="card w-5112">
+  <div className="card-body">
+    <h5 className="basic1">ENTERPRISE</h5>
+
+        <div><h3 className='free1'>-
+</h3></div>
+
+    
+    <a href="https://calendly.com/app/signup" className="btn btn-primary1">
+       Contact Sales
+    </a>
+  </div>
+</div>
+{/* online23 */}
+</div>
+
+
+{/* Button */}
+
+
+<div className="bt12">
+<button className='core_feature'><h3 className='Core'>Core Features</h3></button>
+  </div>
+
+  <div className="bt12">
+<button className='core_feature'><h3 className='Core'>Teams</h3></button>
+  </div>
+
+  <div className="bt12">
+<button className='core_feature'><h3 className='Core'>Customization</h3></button>
+  </div>
+
+  <div className="bt12">
+<button className='core_feature'><h3 className='Core'>Integrations</h3></button>
+  </div>
+
+  <div className="bt12">
+<button className='core_feature'><h3 className='Core'>Business Support</h3></button>
+  </div>
+
+
+{/* Frequently asked Q */}
+
+<div className='Q'>
+
+ <div className='topw'><h2 className='Questions'>Frequently Asked Questions</h2></div>
+  
+
+
+ 
+<div className='one_block'>
+
+<div className="bt13">
+<button className='What'><h3 className='happens'>What happens at the end of my trial?</h3></button>
+  </div>
+
+  <br/>
+  <div className="bt13">
+<button className='What'><h3 className='happens'>Can we try Calendly with multiple users?</h3></button>
+  </div>
+
+  <br/>
+  <div className="bt13">
+<button className='What'><h3 className='happens'>What does the renewal process look like?</h3></button>
+  </div>
+
+  <br/>
+  <div className="bt13">
+<button className='What'><h3 className='happens'>How do I upgrade or downgrade?</h3></button>
+  </div>
+
+  <br/>
+  <div className="bt13">
+<button className='What'><h3 className='happens'>We’re a non-profit organization, is there special pricing available?</h3></button>
+  </div>
+
+  <br/>
+  <div className="bt13">
+<button className='What'><h3 className='happens'>What payment methods do you accept?</h3></button>
+  </div>
+  
+
+  
+
+</div>
+</div>
+
+
+{/* Easy Access for looking */}
+
+<div className='Box2'>
+
+<div><h2 className='big-size'>Easy access for easy bookings</h2></div>
+
+<div className='Share'><p className='your'>Share your Calendly link right from your browser, so you can schedule meetings without the back-and-forth</p></div>
+
+<div className="plans">
+<button className='See_our_plans'><p className='style1'>Start  for free</p></button>  
+<button className='Start_For_free' src='/' alt="/"><p className='Talk'>Talk with  sales</p></button>
+
+  </div>
+  
+
+</div>
+
+
+
+
+
+
+
+    </>
   )
 }
 
